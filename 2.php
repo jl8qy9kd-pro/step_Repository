32203c345499d0a950b3f9d04579558041cdf9c5
Dmitry <nightblender@yandex.ru>
2021-04-15 18:38:50 +0300

    add importXml($a)
    Реализовать функцию importXml($a). $a – путь к xml файлу (структура файла приведена ниже). Результат ее выполнения: прочитать файл $a и импортировать его в созданную БД.

--- 2.php
+++ 2.php
@@ -38,3 +38,61 @@ return $a;
 } 
 echo "<pre>";
 //print_r(mySortForKey([['a'=>2,'b'=>1], ['a'=>5], ['b'=>3], ['a'=>6], ['a'=>1,'b'=>2], ['a'=>8]], b));
+
+
+function importXml($a) {
+$user='root';
+$pass='root';
+$pdoConn = new PDO('mysql:host=localhost;dbname=test_samson', $user, $pass);
+
+$inPutXML= $a;
+  
+for ($i = 0, $parent_id = 0, $codeCategory = 1; $i < $inPutXML->Товар->count(); $i++) 
+   {
+    $addProduct = $pdoConn->prepare("INSERT INTO `a_product`(`ид_продукта`, `код_продукта`, `название`)  VALUES (NULL,?,?)");
+    if ($inPutXML->Товар[$i]->attributes()->{'Код'}) 
+     {
+       $addProduct->execute([$inPutXML->Товар[$i]->attributes()->{'Код'}, $inPutXML->Товар[$i]->attributes()->{'Название'}]);
+     }else 
+         {
+           $insert_product->execute([NULL, $inPutXML->Товар[$i]->attributes()->{'Название'}]);
+         }
+    $product_id = $pdoConn->lastInsertId();
+    $addPrice = $pdoConn->prepare("INSERT INTO `a_price`(`ид_продукта`, `тип`, `цена`) VALUES ($product_id,?,?)");
+    foreach($inPutXML->Товар[$i]->Цена as $price) 
+          {
+            $addPrice->execute([$price['Тип'], $price]); 
+          }
+    $addProperties = $pdoConn->prepare("INSERT INTO `a_property`(`ид_продукта`, `название`, `свойство`)  VALUES ($product_id,?,?)");
+    foreach($inPutXML->Товар[$i]->Свойства as $properties)
+          {  
+            foreach($properties as $property => $value) 
+                  {
+                  if($inPutXML->Товар[$i]->Свойства) 
+                   {
+                 $addProperties->execute([$property, $value]);
+               }
+              }
+          }
+    $addCategories = $pdoConn->prepare("INSERT INTO `a_category`(`ид_категории`, `код_категории`, `название`, `ид_родителя`) VALUES  (NULL,?,?,?)");
+    $combiConnect = $pdoConn->prepare("INSERT INTO `combiconnect`(`ид_продукта`, `ид_категории`) VALUES (?, ?)");
+    foreach ($inPutXML->Товар[$i]->Разделы->Раздел as $category_name) 
+          {
+            $query_category = $pdoConn->query("SELECT * FROM a_category WHERE название LIKE '$category_name'");
+            $category = $query_category->fetch(PDO::FETCH_ASSOC);
+            if ($category['название'] == $category_name) 
+             {
+               $parent_id = $category['ид_категории'];
+               $combiConnect->execute([$product_id, $parent_id]);   
+             }else 
+                 {
+             $addCategories->execute([$codeCategory, $category_name, $parent_id]);
+             $parent_id = $pdoConn->lastInsertId();
+             $combiConnect->execute([$product_id, $pdoConn->lastInsertId()]);
+             $codeCategory++;
+         }  
+      }
+   }
+}
+
+//importXml(simplexml_load_file('C:\Users\Gomynkyl\Desktop\git\InPutXML.xml'));
