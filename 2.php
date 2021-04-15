dc5176f4896e7f38187b6f1a78bb2f86c74551d4
Dmitry <nightblender@yandex.ru>
2021-04-15 18:35:30 +0300

    add mySortForKey($a, $b)
    функию mySortForKey($a, $b). $a – двумерный массив вида [['a'=>2,'b'=>1],['a'=>1,'b'=>3]], $b – ключ вложенного массива. Результат ее выполнения: двумерном массива $a отсортированный по возрастанию значений для ключа $b. В случае отсутствия ключа $b в одном из вложенных массивов, выбросить ошибку класса Exception с индексом неправильного массива.

--- 2.php
+++ 2.php
@@ -12,3 +12,29 @@ function convertString($a, $b)
        } 
 } 
 //print_r(convertString('aKW K W ukwKWKW','KW'));
+
+function mySortForKey($a, $b)
+{
+  uasort($a, function ($min, $max) use ($b)
+            {
+             return ($min["$b"] - $max["$b"]);
+            }      
+        );
+for($i = 0; $i < count($a); $i++) 
+  { 
+    try
+      {
+        if(!array_key_exists("$b", $a[$i])) 
+         {
+           throw new Exception("Ключ ($b) отсутствует в массиве с индексом: "."$i"."<br>");
+         } 
+      }catch(Exception $e)
+           {
+             echo $e -> getMessage();
+           }
+  }
+  echo "Массив отсортирован по ключу ($b) <br>";
+return $a;
+} 
+echo "<pre>";
+//print_r(mySortForKey([['a'=>2,'b'=>1], ['a'=>5], ['b'=>3], ['a'=>6], ['a'=>1,'b'=>2], ['a'=>8]], b));
