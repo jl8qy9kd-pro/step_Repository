53f33075f087a8b590995c789772446ed46e33fe
Dmitry <nightblender@yandex.ru>
2021-05-28 01:32:29 +0300

    wwwBitrixTask3

--- www/index.php
+++ www/index.php
@@ -2,29 +2,106 @@
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Мебельная компания");
 ?><p>
-Наша компания существует на Российском рынке с 1992 года. За это время «Мебельная компания» прошла большой путь от маленькой торговой фирмы до одного из крупнейших производителей корпусной мебели в России.
-</p><p>
-«Мебельная компания» осуществляет производство мебели на высококлассном оборудовании с применением минимальной доли ручного труда, что позволяет обеспечить высокое качество нашей продукции. Налажен производственный процесс как массового и индивидуального характера, что с одной стороны позволяет обеспечить постоянную номенклатуру изделий и индивидуальный подход – с другой.
-<h3>Наша продукция</h3>
-<?$APPLICATION->IncludeComponent("bitrix:furniture.catalog.index", "", array(
-	"IBLOCK_TYPE" => "products",
-	"IBLOCK_ID" => "2",
-	"IBLOCK_BINDING" => "section",
-	"CACHE_TYPE" => "A",
-	"CACHE_TIME" => "36000000",
-	"CACHE_GROUPS" => "N"
-	),
-	false
-);?>
-<h3>Наши услуги</h3>
-<?$APPLICATION->IncludeComponent("bitrix:furniture.catalog.index", "", array(
-	"IBLOCK_TYPE" => "products",
-	"IBLOCK_ID" => "3",
-	"IBLOCK_BINDING" => "element",
-	"CACHE_TYPE" => "A",
-	"CACHE_TIME" => "36000000",
-	"CACHE_GROUPS" => "N"
-	),
-	false
-);?>
+		<div class="main_container homepage">
+			
+			<!-- events -->
+			<div class="ev_events">
+				<div class="ev_h">
+					<h3>Ближайшие события</h3>
+					<a href="" class="ev_allevents">Все мероприятия &rarr;</a>
+				</div>
+				<ul class="ev_lastevent">
+					<li>
+						<h4><a href="">29 августа 2012, Москва</a></h4>
+						<p>Семинар производителей мебели России и СНГ, Обсуждение тенденций.</p>
+					</li>
+					<li>
+						<h4><a href="">30 августа 2012, Санкт-Петербург</a></h4>
+						<p>Открытие шоу-рума на Невском проспекте. Последние модели в большом ассортименте.</p>
+					</li>
+					<li>
+						<h4><a href="">31 августа 2012, Краснодар</a></h4>
+						<p>Открытие нового магазина в нашей дилерской сети.</p>
+					</li>
+				</ul>
+				<div class="clearboth"></div>
+			</div>
+			<!-- // end events -->
+			<div class="cn_hp_content">
+				<div class="cn_hp_category">
+					<ul>
+						<li>
+							<img src="/bitrix/templates/.default/content/1.png" alt=""/>
+							<h2><a href="">Мягкая мебель</a></h2>
+							<p>Диваны, кресла и прочая мягкая мебель <a class="cn_hp_categorymore" href="">&rarr;</a></p>
+							<div class="clearboth"></div>
+						</li>
+						<li>
+							<img src="/bitrix/templates/.default/content/2.png" alt=""/>
+							<h2><a href="">Офисная мебель</a></h2>
+							<p>Диваны, столы, стулья <a class="cn_hp_categorymore" href="">&rarr;</a></p>
+							<div class="clearboth"></div>
+						</li>
+						<li>
+							<img src="/bitrix/templates/.default/content/3.png" alt=""/>
+							<h2><a href="">Мебель для кухни</a></h2>
+							<p>Полки, ящики, столы и стулья <a class="cn_hp_categorymore" href="">&rarr;</a></p>
+							<div class="clearboth"></div>
+						</li>
+						<li>
+							<img src="/bitrix/templates/.default/content/4.png" alt=""/>
+							<h2><a href="">Детская мебель</a></h2>
+							<p>Кровати, стулья, мягкая детская мебель <a class="cn_hp_categorymore" href="">&rarr;</a></p>
+							<div class="clearboth"></div>
+						</li>
+					</ul>
+					<a href="" class="cn_hp_category_more">Все разделы каталога &rarr;</a>
+				</div>
+				<div class="cn_hp_post">
+					<div class="cn_hp_post_new">
+						<h3>Новинки</h3>
+						<img src="/bitrix/templates/.default/content/7.png" alt=""/>
+						<p>Угловой диван "Титаник", с большим выбором расцветок и фактур.</p>
+						<div class="clearboth"></div>
+					</div>
+					<div class="cn_hp_post_action">
+						<h3>Акции</h3>
+						<img src="/bitrix/templates/.default/content/7.png" alt=""/>
+						<p>Угловой диван "Титаник", с большим выбором расцветок и фактур.</p>
+						<div class="clearboth"></div>
+					</div>
+					<div class="cn_hp_post_bestsellersn">
+						<h3>Хиты продаж</h3>
+						<img src="/bitrix/templates/.default/content/7.png" alt=""/>
+						<p>Угловой диван "Титаник", с большим выбором расцветок и фактур.</p>
+						<div class="clearboth"></div>
+					</div>
+				</div>
+				<div class="cn_hp_lastnews">
+					<h3><a href="">Новости</a></h3>		
+					<ul>
+						<li>
+							<h4><a href="">29 августа 2012</a></h4>
+							<p>Поступление лучших офисных кресел из Германии</p>
+						</li>
+						<li>
+							<h4><a href="">29 августа 2012</a></h4>
+							<p>Мастер-класс дизайнеров из Италии для производителей мебели</p>
+						</li>
+						<li>
+							<h4><a href="">29 августа 2012</a></h4>
+							<p>Открытие нашего нового офиса рядом с метро Измайлово</p>
+						</li>
+						<li>
+							<h4><a href="">29 августа 2012</a></h4>
+							<p>Наша дилерская сеть расширилась теперь ассортимент наших товаров доступен в Казани</p>
+						</li>					
+					</ul>
+					<br/>
+					<a href="" class="cn_hp_lastnews_more">Все новости &rarr;</a>
+				</div>
+				<div class="clearboth"></div>
+			</div>
+		</div>
+		
 </p><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
