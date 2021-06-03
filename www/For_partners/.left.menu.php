f44a1e4eba6718ac36c57e37d356c9c40a3b0f4a
Dmitry <nightblender@yandex.ru>
2021-06-03 13:51:29 +0300

    2. Структура Bitrix Framework

---
+++ www/For_partners/.left.menu.php
@@ -0,0 +1,25 @@
+<?
+$aMenuLinks = Array(
+	Array(
+		"Условия сотрудничества", 
+		"/For_partners/usloviya-sotrudnichestva.php", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Расписание мероприятий", 
+		"/For_partners/raspisanie-meropriyatiy/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Как стать партнером", 
+		"/For_partners/kak-stat-partnerom.php", 
+		Array(), 
+		Array(), 
+		"" 
+	)
+);
+?>
---
+++ www/For_partners/kak-stat-partnerom.php
@@ -0,0 +1,7 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetPageProperty("ROBOTS", "_");
+$APPLICATION->SetPageProperty("keywords", "заявка, партнерский договор");
+$APPLICATION->SetPageProperty("description", "Порядок подачи заявки на партнерство и бланк партнерского договора");
+$APPLICATION->SetTitle("Как стать партнером");
+?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
