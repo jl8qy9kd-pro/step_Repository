c1f2002323296d478a6017abbf95c03defd77afd
Dmitry <nightblender@yandex.ru>
2021-06-05 15:25:08 +0300

    4.2 Компоненты. Часть 2

--- www/test.php
+++ www/test.php
@@ -2,18 +2,12 @@
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Новая страница");
 ?><?$APPLICATION->IncludeComponent(
-	"bitrix:menu",
-	"",
+	"bitrix:system.auth.form",
+	"auth",
 	Array(
-		"ALLOW_MULTI_SELECT" => "N",
-		"CHILD_MENU_TYPE" => "left",
-		"DELAY" => "N",
-		"MAX_LEVEL" => "2",
-		"MENU_CACHE_GET_VARS" => array(""),
-		"MENU_CACHE_TIME" => "3600",
-		"MENU_CACHE_TYPE" => "N",
-		"MENU_CACHE_USE_GROUPS" => "Y",
-		"ROOT_MENU_TYPE" => "top",
-		"USE_EXT" => "Y"
+		"FORGOT_PASSWORD_URL" => "/user/",
+		"PROFILE_URL" => "/user/profile.php",
+		"REGISTER_URL" => "/user/registration.php",
+		"SHOW_ERRORS" => "Y"
 	)
 );?><br><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
