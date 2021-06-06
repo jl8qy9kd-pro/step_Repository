98605e17f03fc69a5b8d92dcd2e5896eba7f7053
Dmitry <nightblender@yandex.ru>
2021-06-07 01:18:22 +0300

    6. Вывод динамической информации

--- www/test.php
+++ www/test.php
@@ -1,13 +1,4 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Новая страница");
-?><?$APPLICATION->IncludeComponent(
-	"bitrix:system.auth.form",
-	"auth",
-	Array(
-		"FORGOT_PASSWORD_URL" => "/user/",
-		"PROFILE_URL" => "/user/profile.php",
-		"REGISTER_URL" => "/user/registration.php",
-		"SHOW_ERRORS" => "Y"
-	)
-);?><br><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+?><br><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
