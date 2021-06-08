3c9d7d1e0ecbf2c170d4baa78033046b1d2664a7
Dmitry <nightblender@yandex.ru>
2021-06-08 20:23:57 +0300

    10. Поиск

--- www/test.php
+++ www/test.php
@@ -1,4 +1,8 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Новая страница");
-?><br><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+?><?$APPLICATION->IncludeComponent("bitrix:search.form", "search", Array(
+	
+	),
+	false
+);?><br><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
